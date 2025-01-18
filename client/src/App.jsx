1625a8b8065c1ab64714f3fce968789cdc65c4af
Rotem Mizrachi <rotem.mizrachi@post.runi.ac.il>
2025-01-18 22:03:24 +0200

    added logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,12 @@ import backgroundImage from './assets/background-image.jpg';
 import Loading from "./components/Loading.jsx";
 import { useAuth } from "./AuthProvider";
 function App() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn,setIsLoggedIn } = useAuth();
+
+  const handleLogout = () => {
+    setIsLoggedIn(false); // Set isLoggedIn to false
+    navigate("/login"); // Redirect to the login page
+    };
   return (
       <div className={styles.app}
         style={{
@@ -39,9 +44,10 @@ function App() {
         <header className={styles.appHeader}>
           <img src={projectLogo} alt="Logo" className={styles.appLogo} />
           <nav className={styles.appNav}>
-            <Link to="/home" className={styles.appLink}>Home</Link>
-            <Link to="/" className={styles.appLink}>Login</Link>
-            <Link to="/signup" className={styles.appLink}>Sign Up</Link>
+            <Link to="/" className={styles.appLink}>Home <span style={{ marginLeft: "5px" }}>🏠</span></Link>
+            {!isLoggedIn && (<Link to="/login" className={styles.appLink}>Login</Link>)}
+            {!isLoggedIn && (<Link to="/signup" className={styles.appLink}>Sign Up</Link>)}
+            <Link to="/quiz" className={styles.appLink}>Quiz</Link>
             <Link
                 to={isLoggedIn ? "/chatbot" : "#"}
                 className={styles.appLink}
@@ -49,11 +55,14 @@ function App() {
                 Chat Bot
                 {!isLoggedIn && <span>🔒</span>}
             </Link>            
-            <Link to={isLoggedIn ? "/profile" : "#"} className={styles.appLink}>Profile
-            {!isLoggedIn && <span>🔒</span>}</Link>
+            {isLoggedIn && (<Link to={isLoggedIn ? "/profile" : "#"} className={styles.appLink}>Profile
+            {!isLoggedIn && <span>🔒</span>}</Link>)}
             <Link to="/professional-support" className={styles.appLink}>Professional-Support</Link>
             <Link to={isLoggedIn ? "/forum" : "#"} className={styles.appLink}>Forum
             {!isLoggedIn && <span>🔒</span>}</Link>
+            {isLoggedIn && (<button onClick={handleLogout} className={styles.appLink}>
+                        Logout
+            </button>)}
           </nav>
         </header>
         <Suspense fallback={<Loading />}>
@@ -62,9 +71,10 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/chatbot" element={<ProtectedRoute><BotPage /></ProtectedRoute>} />          
               <Route path="/profile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-              <Route path="/" element={<LoginPage />} />
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
+              <Route path="/quiz" element={<QuizPage />}/>
               <Route path="/forum" element={<ProtectedRoute><ForumPage /></ProtectedRoute>} />
               <Route path="/professional-support" element={< ProfessionalSupportPage />} />
             </Routes>
